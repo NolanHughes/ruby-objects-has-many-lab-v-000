4e1daad24051d18ff82082d82a30b21a07fa76c5
Nolan Hughes <nhughes987@gmail.com>
2017-11-09 05:59:31 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,11 +13,13 @@ class Author
   end
 
   def add_post(post)
+    @@post_count += 1
     @posts << post
     post.author = self
   end
 
   def add_post_by_title(name)
+    @@post_count += 1
     post = Post.new(name)
     @posts << post
     post.author = self
